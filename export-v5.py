48c22dad2e4a63f4618f2a6dd08962a41d8e9750
sajjan72 <sajjansingh72@yahoo.com>
2017-09-24 13:39:58 +0530

    v5

---
+++ export-v5.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import folium
+import MySQLdb
+db = MySQLdb.connect("localhost","root","password","testdb" )
+from folium.plugins import Draw, Fullscreen
+cursor = db.cursor()
+m = folium.Map(location=[-27.228989, -48.36525], zoom_start=12)
+
+draw = Draw(export=True)
+
+draw.add_to(m)
+
+m
+
+
+# In[1]:
+
+
+import os
+import geopandas
+
+
+gpd = geopandas.read_file(
+    os.path.join(os.path.expanduser('~'), 'data.geojson')
+)
+
+gpd['geometry'][0]
+ try:
+    cursor.execute("""INSERT INTO anooog1 VALUES (%s)""",('geometry'][0]))
+    db.commit()
+ except:     
+     db.rollback()
+ 
+
+# In[2]:
+
+
+gpd.__geo_interface__
